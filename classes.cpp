f6bd47bda14d0f3417fb3bd3b10759e83ec43c55
Luis Melendez MacStation <luis_melendez_mac@gmail.com>
2023-04-05 08:17:45 -0600

    Classes introduction

---
+++ classes.cpp
@@ -0,0 +1,91 @@
+#include <cstdio>
+#include <cstddef>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+struct Clock
+{
+    void add_year()
+    {
+        year++;
+    }
+
+    int get_year()
+    {
+        return year;
+    }
+
+    private:
+    int year;
+
+};
+
+class Setting
+{
+    int id;
+    string title;
+    string summary;
+
+    public:
+
+    Setting()
+    {
+        id = 0;
+        title = "no title";
+        summary = "her to describe something";
+    }
+
+    void setId(int id)
+    {
+        this->id = id;
+    }
+
+    int getId()
+    {
+        return id;
+    }
+
+    void printState()
+    {
+        cout << "id = " << id << endl;
+        cout << "title = " << title << endl;
+        cout << "summary = " << summary << endl;
+    }
+
+
+};
+
+
+struct ProcessBlock
+{
+    int id;
+    int parendId;
+    int ip;
+    double time;
+};
+
+int main()
+{
+
+Clock clock;
+clock.add_year();
+clock.add_year();
+cout << clock.get_year() << endl;
+
+Setting idiom;
+idiom.printState();
+
+ProcessBlock processBlock = {2,1,0x454545,3.1416 };
+cout << processBlock.time << endl;
+
+
+int a_1[] = {1,2,3};
+int a_2[5] = {};
+int a_3[5] ={1,2,3,4,5};
+
+
+return 0; 
+}
